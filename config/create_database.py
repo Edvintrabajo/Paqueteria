16f625bf22741554ae26f9f865c7392fd47e512a
Edvintrabajo <Edvintrabajo@gmail.com>
2022-05-18 20:39:50 +0100

    añadiendo creacion de base de datos

---
+++ config/create_database.py
@@ -0,0 +1,9 @@
+import sqlite3
+def create_database(db_file):
+    conn = sqlite3.connect(db_file) # Warning: This file is created in the current directory
+    conn.execute("CREATE TABLE todo (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)")
+    conn.execute("INSERT INTO todo (task,status) VALUES ('Read A-byte-of-python to get a good introduction into Python',0)")
+    conn.execute("INSERT INTO todo (task,status) VALUES ('Visit the Python website',1)")
+    conn.execute("INSERT INTO todo (task,status) VALUES ('Test various editors for and check the syntax highlighting',1)")
+    conn.execute("INSERT INTO todo (task,status) VALUES ('Choose your favorite WSGI-Framework',0)")
+    conn.commit()
