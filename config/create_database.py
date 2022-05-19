5591cc89d85cc93b60943def6a374d2e1b8ae24d
Miguel Ángel Medina Ramírez <migueliberto86@gmail.com>
2022-05-19 20:50:23 +0100

    cambios a la creacion de tablas

--- config/create_database.py
+++ config/create_database.py
@@ -1,6 +1,6 @@
 import sqlite3
 def create_database(db_file):
-    conn = sqlite3.connect(db_file) # Warning: This file is created in the current directory
+    conn = sqlite3.connect(db_file) Warning: This file is created in the current directory
    
 
     conn.execute('''CREATE TABLE IF NOT EXISTS 'Producto' (
@@ -9,9 +9,59 @@ def create_database(db_file):
         'CantidadProducto' INT NOT NULL CHECK(CantidadProducto>0), 
         'PesoProducto' DEC(4 , 2) NOT NULL CHECK(PesoProducto>0))''')
 
-    # conn.execute("INSERT INTO Cliente (DNIcliente, Nombre, Apellidos, DireccionCliente) VALUES ('12345678A', 'Carlos', 'Freyer', 'Las Palmas')")
-    # conn.execute("INSERT INTO Cliente (DNIcliente, Nombre, Apellidos, DireccionCliente) VALUES ('12345678B', 'Mario', 'Alonso', 'Telde')")
-    # conn.execute("INSERT INTO Cliente (DNIcliente, Nombre, Apellidos, DireccionCliente) VALUES ('12345678C', 'Richard', 'Ramirez', 'Galdar')")
+    conn.execute('''CREATE TABLE IF NOT EXISTS "Cliente" (
+	"DNIcliente"	VARCHAR(9),
+	"Nombre"	VARCHAR(30) NOT NULL,
+	"Apellidos"	VARCHAR(30) NOT NULL,
+	"DireccionCliente"	VARCHAR(50) NOT NULL,
+	PRIMARY KEY("DNIcliente"))''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS "Oficinista" (
+	"ID_Oficinista"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"Nombre"	VARCHAR(30) NOT NULL)''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS "Repartidor" (
+	"DNIRepartidor"	VARCHAR(9),
+	"NombreRepartidor"	VARCHAR(30) NOT NULL,
+	PRIMARY KEY("DNIRepartidor"))''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS "Pedido" (
+	"IDPedido"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"PesoTotal"	DEC(5 , 2),
+	"CosteTotal"	DEC(6 , 2),
+	"Distancia"	INTEGER NOT NULL CHECK(Distancia>0),
+	"DireccionEnvio"	VARCHAR(50) NOT NULL,
+	"Estado"	TEXT NOT NULL DEFAULT 'A' CHECK(Estado IN ('A','S','E','I')),
+	"DNIRepartidor"	VARCHAR(9) NOT NULL,
+	"DNIcliente"	VARCHAR(9) NOT NULL,
+	FOREIGN KEY("DNIRepartidor") REFERENCES "Repartidor"("DNIRepartidor"),
+	FOREIGN KEY("DNIcliente") REFERENCES "Cliente"("DNIcliente"))''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS "Oficinista_Pedido" (
+	"ID_Oficinista"	INTEGER,
+	"IDPedido"	INTEGER,
+	FOREIGN KEY("ID_Oficinista") REFERENCES "Oficinista"("ID_Oficinista"),
+	FOREIGN KEY("IDPedido") REFERENCES "Pedido"("IDPedido"),
+	PRIMARY KEY("IDPedido"))''')
+
+    conn.execute('''CREATE TABLE IF NOT EXISTS "Pedido_Producto" (
+	"ID_Pedido_Producto"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"IDPedido"	INTEGER,
+	"IDProducto"	INTEGER,
+	FOREIGN KEY("IDProducto") REFERENCES "Producto"("IDProducto"),
+	FOREIGN KEY("IDPedido") REFERENCES "Pedido"("IDPedido"))''')
+
+
+
+
+
+
+
+    # conn.execute("""INSERT INTO Cliente (DNIcliente, Nombre, Apellidos, DireccionCliente) VALUES 
+    # ('12345678A', 'Carlos', 'Freyer', 'Las Palmas'),
+    # ('12345678B', 'Mario', 'Alonso', 'Telde'),
+    # ('12345678C', 'Richard', 'Ramirez', 'Galdar')""")
+    
     
     # conn.execute("CREATE TABLE Oficinista (ID_Oficinista INTEGER PRIMARY KEY AUTOINCREMENT, Nombre char(30) NOT NULL)")
     # conn.execute("INSERT INTO Oficinista (Nombre) VALUES ('Javi')")
