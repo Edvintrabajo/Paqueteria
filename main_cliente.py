b9e226bc703059e2cce2754ec016e9613c138a68
Edvintrabajo <Edvintrabajo@gmail.com>
2022-05-24 16:40:33 +0100

    rutas clientes

--- main_cliente.py
+++ main_cliente.py
@@ -1,9 +1,7 @@
-import os
 import sys
 sys.path.append('models') # add the models directory to the path
 
-import bottle
-from bottle import route, run, template, request, get, post, redirect, static_file, error, response
+from bottle import run, template, request, get, post, redirect, static_file, error
 from models.clientes import Cliente
 
 cliente = Cliente()
@@ -22,20 +20,23 @@ def new_task_save():
             'Apellidos': request.POST.Apellido.strip(),
             'DireccionCliente': request.POST.Direccion.strip()
         }
+
+        if data.get('DNIcliente') == "" or data.get('Nombre') == "" or data.get('Apellidos') == "" or data.get('DireccionCliente') == "":
+            return redirect("/error")
+
         cliente.insert(data)
 
         # se muestra el resultado de la operación
         return redirect('/cliente')
 
-
-@get('/edit_cliente/<no:varchar(9)>')
+@get('/edit_cliente/<no>')
 def edit_item_form(no):
     fields = ['Nombre', 'Apellidos', 'DireccionCliente']
     where = {'DNIcliente': no}
     cur_data = cliente.get(fields, where)  # get the current data for the item we are editing
-    return template('edit_task', old=cur_data, no=no)
+    return template('edit_clientes', old=cur_data, no=no)
 
-@post('/edit_cliente/<no:varchar(9)>')
+@post('/edit_cliente/<no>')
 def edit_item(no):
     
     if request.POST.save:
@@ -44,15 +45,40 @@ def edit_item(no):
             'Apellidos': request.POST.Apellido.strip(),
             'DireccionCliente': request.POST.Direccion.strip()
         }
+        if data.get('Nombre') == "":
+            del data['Nombre']
+
+        if data.get('Apellidos') == "":
+            del data['Apellidos']
+
+        if data.get('DireccionCliente') == "":
+            del data['DireccionCliente']
+
         where = {'DNIcliente': no}
         
         cliente.update(data, where)
         
     return redirect('/cliente')
 
+@get('/delete_cliente/<no>')
+def delete_item_form(no):
+    fields = ['DNIcliente']
+    where = {'DNIcliente': no}
+    cur_data = cliente.get(fields, where)  # get the current data for the item we are editing
+    return template('delete_clientes', old=cur_data, no=no)
 
+@post('/delete_cliente/<no>')
+def delete_item(no):
+    
+    if request.POST.delete:
+        where = {'DNIcliente': no}
+        cliente.delete(where)
 
+    return redirect('/cliente')
 
+@error(404)
+def error404(error):
+    return static_file('404.html', root='static')
 
 if __name__ == '__main__':
 
